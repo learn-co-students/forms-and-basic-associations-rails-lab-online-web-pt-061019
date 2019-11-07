bb76600c629abe5bc8b1bf108c7b7de1a14ee359
ZumbaIdol <david.a.foote@gmail.com>
2019-11-07 15:34:40 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,4 +11,12 @@ class Song < ActiveRecord::Base
   def artist_name
     self.artist ? self.artist.name : nil
   end
+
+  def genre=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
 end
